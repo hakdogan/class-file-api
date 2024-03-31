3f534286570dd6aaabf1bec8b42abf9b33dd63de
Hüseyin Akdoğan <hakdogan75@gmail.com>
2024-03-31 17:33:22 +0300

    run.sh added

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+javac -d output --release 22 --enable-preview --source-path "./*/src" \
+$(find . -name "*.java")
+
+cd output
+
+java -cp . --enable-preview org/jugistanbul/GenerateClass
+java HelloJEP457
+
+echo
+java -cp . --enable-preview org/jugistanbul/ParseClass
+
+
+
+
